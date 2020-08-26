e32cd4c4027ff48f021f28c2b4243e09bbb4d73e
arthurnog <artuts874@gmail.com>
2020-08-26 19:52:49 -0300

    adicionando nodemon ao projeto

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ app.get('/projects',(request,response) => {
     return response.json({ message: 'ohayo sekai good morning world'});
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('BckNd started (╯°□°）╯︵ ┻━┻');
+});
