a43657a1739b47bbc90193a44bc00fdf69d813fb
arthurnog <artuts874@gmail.com>
2020-08-26 22:45:02 -0300

    métodos http

--- src/index.js
+++ src/index.js
@@ -3,7 +3,33 @@ const express = require('express');
 const app = express();
 
 app.get('/projects',(request,response) => {
-    return response.json({ message: 'ohayo sekai good morning world'});
+    return response.json([
+        'proj1',
+        'proj2',
+    ]);
+});
+
+app.post('/projects',(request,response) => {
+    return response.json([
+        'proj1',
+        'proj2',
+        'proj3',
+    ]);
+});
+
+app.put('/projects/:id',(request,response) => {
+    return response.json([
+        'proj4',
+        'proj2',
+        'proj3',
+    ]);
+});
+
+app.delete('/projects/:id',(request,response) => {
+    return response.json([
+        'proj2',
+        'proj3',
+    ]);
 });
 
 app.listen(3333, () => {
